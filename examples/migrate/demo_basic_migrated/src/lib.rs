a8be11fc8e0558d541f4aa8115c471375137f220
Louis Maddox <louismmx@gmail.com>
2025-11-12 22:44:02 +0000

    chore: basic migration demo (incorrect)

--- examples/migrate/demo_basic_migrated/src/lib.rs
+++ examples/migrate/demo_basic_migrated/src/lib.rs
@@ -1,9 +1,1 @@
-//! A lib.rs module containing one public enum.
-
-/// A public enum
-pub enum TimeOfDay {
-    /// An enum variant called Day
-    Day,
-    /// An enum variant called Night
-    Night,
-}
+# ! [doc = " A lib.rs module containing one public enum."] pub enum TimeOfDay { Day , Night , }
--- examples/migrate/demo_basic_migrated/src/main.rs
+++ examples/migrate/demo_basic_migrated/src/main.rs
@@ -1,13 +1,1 @@
-//! A main.rs module with a main function that prints good morning
-
-use cli_demo_basic_migrated::TimeOfDay;
-
-/// A function that prints a hello world message.
-fn main() {
-    let current = TimeOfDay::Day;
-
-    match current {
-        TimeOfDay::Day => println!("Good morning! It’s a bright new day."),
-        TimeOfDay::Night => println!("Good evening! Time to relax under the stars."),
-    }
-}
+# ! [doc = " A main.rs module with a main function that prints good morning"] use cli_demo_basic_migrated :: TimeOfDay ; fn main () { let current = TimeOfDay :: Day ; match current { TimeOfDay :: Day => println ! ("Good morning! It’s a bright new day.") , TimeOfDay :: Night => println ! ("Good evening! Time to relax under the stars.") , } }
