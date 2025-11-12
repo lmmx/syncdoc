d42297c2f69061d135449f5e2a33f459f57813cb
Louis Maddox <louismmx@gmail.com>
2025-11-12 21:17:48 +0000

    feat: example of a migration using the CLI

---
+++ examples/migrate/demo_basic/README.md
@@ -0,0 +1,1 @@
+An example of how to use this crate's CLI to migrate (before and after)
---
+++ examples/migrate/demo_basic/src/lib.rs
@@ -0,0 +1,9 @@
+//! A lib.rs module containing one public enum.
+
+/// A public enum
+pub enum TimeOfDay {
+    /// An enum variant called Day
+    Day,
+    /// An enum variant called Night
+    Night,
+}
