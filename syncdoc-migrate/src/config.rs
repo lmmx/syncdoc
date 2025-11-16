8fb6974779ab1c7310b39bd7d67d8f2a93df8bad
Louis Maddox <louismmx@gmail.com>
2025-11-16 13:45:37 +0000

    docs: backtick escape the casual use of syntax

--- syncdoc-migrate/src/config.rs
+++ syncdoc-migrate/src/config.rs
@@ -2,10 +2,10 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum DocsPathMode {
     /// Path configured in Cargo.toml [package.metadata.syncdoc]
-    /// Macros will be called without path arguments: #[omnidoc], module_doc!()
+    /// Macros will be called without path arguments: `#[omnidoc]`, `module_doc!()`
     TomlConfig,
 
     /// Path specified inline in each macro call
-    /// Macros will include path: #[omnidoc(path = "docs")], module_doc!(path = "docs")
+    /// Macros will include path: `#[omnidoc(path = "docs")]`, `module_doc!(path = "docs")`
     InlinePaths,
 }
